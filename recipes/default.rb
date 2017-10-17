4a0c8a92b8483a1859ee620a7fd1e38806afe3cb
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-10-17 12:26:51 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,9 @@
-#
 # Cookbook:: cloudwatch
 # Recipe:: default
-#
 # Copyright:: 2017, The Authors, All Rights Reserved.
+case node['platform']
+when 'amazon'
+  include_recipe 'cloudwatch::linux'
+when 'ubuntu'
+  include_recipe 'cloudwatch::ubuntu'
+end
