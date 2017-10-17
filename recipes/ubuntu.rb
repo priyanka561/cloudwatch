d23c272ed2ee288e100abcce48438c310725cf4b
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-10-17 12:24:44 +0530

    Create ubuntu.rb

---
+++ recipes/ubuntu.rb
@@ -0,0 +1,18 @@
+# cookbook Name:: cloudwatch
+# Recipe:: default
+# Copyright 2017, REANCLOUD
+execute 'update' do
+  command 'apt-get update -y'
+end
+execute 'curlinstall' do
+  command 'apt-get install curl -y'
+end
+execute 'install' do
+  command 'curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O'
+end
+execute 'start' do
+  command 'service awslogs start'
+end
+execute 'on' do
+  command 'update-rc.d awslogs start 20 2 3 4 5 . stop 20 0 1 6 .'
+end
